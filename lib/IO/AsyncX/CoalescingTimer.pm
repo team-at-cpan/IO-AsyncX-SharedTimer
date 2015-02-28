805e99a26b4e4bb3898e8e03308abb21fa5663bb
Tom Molesworth <tom@perlsite.co.uk>
2015-02-28 18:07:28 +0000

    Basic gettimeofday coalescing
    git-svn-id: http://entitymodel.com/svn/trunk/IO-AsyncX-SharedTimer@3072 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

--- lib/IO/AsyncX/CoalescingTimer.pm
+++ lib/IO/AsyncX/CoalescingTimer.pm
@@ -3,6 +3,8 @@ package IO::AsyncX::CoalescingTimer;
 use strict;
 use warnings;
 
+use parent qw(IO::Async::Notifier);
+
 our $VERSION = '0.001';
 
 =head1 NAME
@@ -13,9 +15,37 @@ IO::AsyncX::CoalescingTimer -
 
 =head1 DESCRIPTION
 
+=cut
+
+use Time::HiRes ();
+use curry::weak;
+
 =head1 METHODS
 
 =cut
+
+sub configure {
+	my ($self, %args) = @_;
+	if(exists $args{resolution}) {
+		$self->{resolution} = delete $args{resolution};
+	}
+	$self->SUPER::configure(%args);
+}
+
+sub resolution { shift->{resolution} }
+
+sub now {
+	my ($self) = @_;
+	$self->{after} ||= $self->loop->delay_future(
+		after => $self->resolution
+	)->on_ready($self->curry::weak::expire);
+	$self->{now} //= Time::HiRes::time;
+}
+
+sub expire {
+	my ($self) = @_;
+	delete @{$self}{qw(now after)};
+}
 
 1;
 
